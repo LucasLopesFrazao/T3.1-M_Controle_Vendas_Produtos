ef52f1d3e9aa162ac9924499c3449ab3688209c4
LucasLopesFrazao <211031771@aluno.unb.br>
2022-07-22 12:34:34 -0300

    first commit

---
+++ CODIGO/ControleVendaProduto/src/modelo/Produto.java
@@ -0,0 +1,56 @@
+package modelo;
+
+public class Produto {
+	
+	private Integer id;
+	private Double preco;
+	private Integer qtdEstoque;
+	private String nome;
+	
+	public Produto() {
+		
+	}
+
+	public Produto(Integer id, Double preco, Integer qtdEstoque, String nome) {
+		this.id = id;
+		this.preco = preco;
+		this.qtdEstoque = qtdEstoque;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQtdEstoque() {
+		return qtdEstoque;
+	}
+
+	public void setQtdEstoque(Integer qtdEstoque) {
+		this.qtdEstoque = qtdEstoque;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void adicionarEstoque(Integer quantidade) {
+		this.qtdEstoque += quantidade;
+	}
+}
---
+++ CODIGO/ControleVendaProduto/src/modelo/Vendedor.java
@@ -0,0 +1,25 @@
+package modelo;
+
+public class Vendedor extends Pessoa{
+
+	private String departamento;
+	
+	public Vendedor() {
+		
+	}
+	
+	public Vendedor(Integer id, String nome, String email, String departamento) {
+		super.setId(id);
+		super.setNome(nome);
+		super.setEmail(email);
+		this.departamento = departamento;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+}
